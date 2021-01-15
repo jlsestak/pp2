ad9f2ec1dc0c00170f18ec3786e2c83ed3efe118
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-01-15 15:21:40 -0500

    Added index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Jessica Sestak and Aaron Utterback
+ * 01/14/2021
+ * http://jsestak.greenriverdev.com/328/pp2/index.php
+ * Pair Program 2
+ */
+
+// turn on error reporting
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+
+echo "PHP Array Practice";
