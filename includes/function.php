c4c1b847f97f28a6253c0962f92aa2d27c215e2a
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-01-17 20:52:26 -0500

    Added largest function

--- includes/function.php
+++ includes/function.php
@@ -1,6 +1,18 @@
 <?php
-function printArray($numbers) {
-    foreach($numbers as $value) {
+function printArray($array) {
+    foreach($array as $value) {
         echo "$value<br>";
     }
 }
+
+function largest($array) {
+    $maxValue = 0;
+    for($i = 0; $i <= sizeof($array); $i++) {
+        if($array[$i] > $maxValue) {
+            $maxValue = $array[$i];
+        }
+    }
+    echo $maxValue;
+}
+
+
